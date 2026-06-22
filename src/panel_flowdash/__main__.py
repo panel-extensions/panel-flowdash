89f1aaae405f9bdf2ff7db95596a53bc08610f46
Simon Høxbro Hansen <hoxbro@protonmail.com>
2026-06-22 16:40:56 +0200

    move __main__ to root for easier access

---
+++ src/panel_flowdash/__main__.py
@@ -0,0 +1,5 @@
+"""Allow running as `python -m panel_flowdash serve`."""
+
+from panel_flowdash.command import main
+
+main()
--- src/panel_flowdash/command/__main__.py
+++
@@ -1,5 +0,0 @@
-"""Allow running as `python -m panel_flowdash.command`."""
-
-from panel_flowdash.command import main
-
-main()
